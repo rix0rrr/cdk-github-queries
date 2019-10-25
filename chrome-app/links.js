706be07924af8768d2996193d33156a8308cb52f
Rico Huijbers <huijbers@amazon.com>
2019-10-25 10:34:44 +0200

    Rename "PRs Created" to imperative "Finish"
    The other phrasing was awkward, this goes better with the rest.

--- chrome-app/links.js
+++ chrome-app/links.js
@@ -3,7 +3,7 @@ const LINKS = [
     { title: "Review", href: "https://github.com/pulls/review-requested" },
     { title: "Triage", href: "https://github.com/aws/aws-cdk/issues?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+assignee%3A{}+label%3Abug+-label%3Ap0+-label%3Ap1+-label%3Ap2+-label%3Ap3+" },
     { title: "Shepherd", href: "https://github.com/pulls/assigned" },
+    { title: "Finish", href: "https://github.com/pulls" },
     { title: "Your bugs", href: "https://github.com/aws/aws-cdk/issues?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+assignee%3A{}+label%3Abug" },
     { title: "Your issues", href: "https://github.com/issues/assigned" },
-    { title: "PRs Created", href: "https://github.com/pulls" },
 ];
