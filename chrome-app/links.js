587406e49b2fb20188722dff05f8c7e6e2690224
Elad Ben-Israel <benisrae@amazon.com>
2019-10-25 13:20:07 +0300

    proposing alternative groupings
    - separate bugs to priority
    - rename "Finish" to "Author"
    - change "Your Issues" to "Non-Bugs"
    - order according to recommended "processing order" (triage => p0 => sheperd => author => review => p1 => p2 => p3 => non-bugs)

--- chrome-app/links.js
+++ chrome-app/links.js
@@ -17,10 +17,13 @@ const ourRepos = ["repo:aws/jsii", "repo:aws/aws-cdk"];
 
 // LINKS
 const LINKS = [
-    { title: "Review", href: "https://github.com/pulls/review-requested" },
     { title: "Triage", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", "-label:p0", "-label:p1", "-label:p2", "-label:p3"])},
+    { title: "P0", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", 'label:p0'])},
     { title: "Shepherd", href: gitHubQuery("https://github.com/pulls", [...pr, "assignee:USERNAME", "-author:USERNAME" ])},
-    { title: "Finish", href: "https://github.com/pulls" },
-    { title: "Your bugs", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug"])},
-    { title: "Your issues", href: "https://github.com/issues/assigned" },
+    { title: "Author", href: "https://github.com/pulls" },
+    { title: "Review", href: "https://github.com/pulls/review-requested" },
+    { title: "P1", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", 'label:p1'])},
+    { title: "P2", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", 'label:p2'])},
+    { title: "P3", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", 'label:p3'])},
+    { title: "Non-bugs", href: gitHubQuery("https://github.com/issues/assigned", [ '-label:bug' ]) },
 ];
