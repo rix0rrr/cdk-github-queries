141259dbc1fcc6eeea9a6beae03f83775a1d2824
Elad Ben-Israel <benisrae@amazon.com>
2019-10-25 14:42:49 +0300

    a few changes based on Rico's feedback

--- chrome-app/links.js
+++ chrome-app/links.js
@@ -17,13 +17,11 @@ const ourRepos = ["repo:aws/jsii", "repo:aws/aws-cdk"];
 
 // LINKS
 const LINKS = [
+    { title: "Review", href: "https://github.com/pulls/review-requested" },
     { title: "Triage", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", "-label:p0", "-label:p1", "-label:p2", "-label:p3"])},
     { title: "P0", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", 'label:p0'])},
     { title: "Shepherd", href: gitHubQuery("https://github.com/pulls", [...pr, "assignee:USERNAME", "-author:USERNAME" ])},
     { title: "Author", href: "https://github.com/pulls" },
-    { title: "Review", href: "https://github.com/pulls/review-requested" },
-    { title: "P1", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", 'label:p1'])},
-    { title: "P2", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", 'label:p2'])},
-    { title: "P3", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", 'label:p3'])},
+    { title: "Other Bugs", href: gitHubQuery("https://github.com/issues", [...issue, ...ourRepos, "assignee:USERNAME", "label:bug", '-label:p0'])},
     { title: "Non-bugs", href: gitHubQuery("https://github.com/issues/assigned", [ '-label:bug' ]) },
 ];
